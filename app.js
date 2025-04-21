988fb2ff6b2c3019ad6c9e2505dff0fe3d91d512
WilliamSalcedo <wsl_1994@hotmail.com>
2025-04-20 19:46:48 -0500

    Se actualizo logica de app.js con cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 // Middlewares
 app.use(bodyParser.json());
+app.use(cors());
 
 //  rutas
 const authRoutes = require("./routes/auth");       // Rutas de login y register
@@ -13,9 +15,13 @@ app.use("/auth", authRoutes);
 
 
 // Conexión a MongoDB
-mongoose.connect("mongodb+srv://adminuser:admin123@cluster0.tgshhiw.mongodb.net/GhostPlay")
-    .then(() => console.log("✅ Conectado a MongoDB Atlas"))
-    .catch(err => console.error("❌ Error al conectar a la BD:", err));
+mongoose.connect("mongodb+srv://ghost_123:ghost123@cluster0.tgshhiw.mongodb.net/GhostPlay?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
+  .catch(err => console.error("❌ Error al conectar a la BD:", err));
+  
 
 // Escuchar peticiones
 app.listen(3012, () => {
